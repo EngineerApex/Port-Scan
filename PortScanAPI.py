9ccfd466b061969b1b3f65aecb67872f8d95e5d6
Engineer Apex <69231443+EngineerApex@users.noreply.github.com>
2024-10-07 23:47:59 +0530

    Update PortScanAPI.py

--- PortScanAPI.py
+++ PortScanAPI.py
@@ -30,9 +30,9 @@ def port_scan():
             return jsonify({"output": "[-] Invalid hostname or IP address"}), 400
 
         print(f"[+] Scanningggg the target: {ip} : nmap version - {nmap.PortScanner().nmap_version()}")
-        #sys.stdout.flush()
+        sys.stdout.flush()
         print("[+] This might take a while...")
-        #sys.stdout.flush()
+        sys.stdout.flush()
 
         # Initialize the Nmap PortScanner
         nm = nmap.PortScanner()
@@ -40,15 +40,17 @@ def port_scan():
         # Scan the target IP with specific arguments
         nm.scan(ip, arguments='-A -Pn -T4')
 
-        
-        for proto in nm[ip].all_protocols():
-            print(f"\nProtocol: {proto}")
-            lport = nm[ip][proto].keys()
-            for port in sorted(lport):
-                state = nm[ip][proto][port]['state']
-                service = nm[ip][proto][port]['name']
-                version = nm[ip][proto][port].get('version', 'N/A')  # Get version if available
-                print(f"{port}/{proto} {state} {service} {version}")
+        if not nm.all_hosts():
+            print("[-] No hosts were found.")
+        else:
+            for proto in nm[ip].all_protocols():
+                print(f"\nProtocol: {proto}")
+                lport = nm[ip][proto].keys()
+                for port in sorted(lport):
+                    state = nm[ip][proto][port]['state']
+                    service = nm[ip][proto][port]['name']
+                    version = nm[ip][proto][port].get('version', 'N/A')  # Get version if available
+                    print(f"{port}/{proto} {state} {service} {version}")
 
     except Exception as e:
         print(f"[-] An error occurred: {e}")
@@ -64,4 +66,4 @@ def port_scan():
     return jsonify({"output": output})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=10000)
+    app.run(debug=True)
